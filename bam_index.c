f2f3968e11eead9ce5601b01890bc2339ff951e9
Charles Plessy <plessy@debian.org>
2010-12-03 13:24:39 +0900

    Imported Upstream version 0.1.12a

--- bam_index.c
+++ bam_index.c
@@ -212,7 +212,7 @@ bam_index_t *bam_index_core(bamFile fp)
 	}
 	if (save_tid >= 0) {
 		insert_offset(idx->index[save_tid], save_bin, save_off, bam_tell(fp));
-		insert_offset(idx->index[save_tid], BAM_MAX_BIN, off_beg, off_end);
+		insert_offset(idx->index[save_tid], BAM_MAX_BIN, off_beg, bam_tell(fp));
 		insert_offset(idx->index[save_tid], BAM_MAX_BIN, n_mapped, n_unmapped);
 	}
 	merge_chunks(idx);
@@ -698,6 +698,7 @@ int bam_fetch(bamFile fp, const bam_index_t *idx, int tid, int beg, int end, voi
 	b = bam_init1();
 	iter = bam_iter_query(idx, tid, beg, end);
 	while ((ret = bam_iter_read(fp, iter, b)) >= 0) func(b, data);
+	bam_iter_destroy(iter);
 	bam_destroy1(b);
 	return (ret == -1)? 0 : ret;
 }
